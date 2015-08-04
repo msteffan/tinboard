ec988d37c4ca5143026aa92dadbf7965f29b915b
Melissa Steffan <msteffan2012@gmail.com>
2015-08-04 14:48:58 -0400

    all js is working correctly to show and hide articles on click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,150 +22,22 @@ $(document).ready(function(){
     $(".columns").click(function(){
         var self = this
         $('h1').each(function(){
-        console.log(self.id);
             if ($(this).hasClass(self.id) == true){
-                // $(this).parent().animate({
-                //         width: "30.6666666667%"
-                //     }, 500, function() {
+                $(this).parent().animate({
+                    width: "30.6666666667%"
+                 }, 500, function() {})
                 $(this).parent().css("display", "block")
                         // Animation complete.
-                    // });
-                }
+            };
             if ($(this).hasClass(self.id) == false){
-                // $('div.one-third.column').animate({
-                //     width: "0"
-                //     }, 500, function() {
+                $(this).parent().animate({
+                     width: "0"
+                }, 500, function() {})
                 //         console.log(this)
                 $(this).parent().css("display", "none")
                      // Animation complete.
             };
-
-
-            // if ($('h1').hasClass(self.id) == true){
-            //     $('div.one-third.column').animate({
-            //         width: "30.6666666667%"
-            //     }, 500, function() {
-            //         $('div.one-third.column').css("display", "block")
-            //         // Animation complete.
-            //     });
-            // }
-        })
-    })
-
-//
-// // two minutes click
-//     $("#twoMinutes").on("click", function(){
-//         console.log(this)
-//         if ($("h1").hasClass("twoMinutes") == true){
-//             $('div.one-third.column').animate({
-//                 width: "30.6666666667%"
-//             }, 500, function() {
-//                 $('div.one-third.column').css("display", "block")
-//                 // Animation complete.
-//             });
-//         }
-//         else
-//         //($("h1").hasClass("twoMinutes") !== true)
-//         {
-//
-//         }
-//     }); // closes #twoMinutes click
-//
-// four mintues click
-    // $("#fourMinutes").on("click", function(){
-    //     console.log(this)
-    //     if ($("h1").hasClass("fourMinutes") == true){
-    //         $('div.one-third.column').animate({
-    //             width: "30.6666666667%"
-    //         }, 500, function() {
-    //             $('div.one-third.column').css("display", "block")
-    //             // Animation complete.
-    //         });
-    //     }
-    //     if ($("h1").hasClass("fourMinutes") != true) {
-    //         console.log("hello")
-    //         $('div.one-third.column').animate({
-    //             width: "0"
-    //         }, 500, function() {
-    //             $('div.one-third.column').css("display", "none")
-    //             // Animation complete.
-    //         });
-    //     }
-    // }); // closes #fourMinutes click
-//
-// // six mintues click
-// $("#sixMinutes").on("click", function(){
-//     console.log(this)
-//     if ($("h1").hasClass("sixMinutes") == true){
-//         $('div.one-third.column').animate({
-//             width: "30.6666666667%"
-//         }, 500, function() {
-//             $('div.one-third.column').css("display", "block")
-//             // Animation complete.
-//         });
-//     }
-//     if ($("h1").hasClass("sixMinutes") != true) {
-//         console.log("hello")
-//         $('div.one-third.column').animate({
-//             width: "0"
-//         }, 500, function() {
-//             $('div.one-third.column').css("display", "none")
-//             // Animation complete.
-//         });
-//     }
-// }); // closes #sixMinutes click
-//
-//
-// // ten mintues click
-//
-// $("#tenMinutes").on("click", function(){
-//     console.log(this)
-//     if ($("h1").hasClass("tenMinutes") == true){
-//         $('div.one-third.column').animate({
-//             width: "30.6666666667%"
-//         }, 500, function() {
-//             $('div.one-third.column').css("display", "block")
-//             // Animation complete.
-//         });
-//     }
-//     if ($("h1").hasClass("tenMinutes") != true) {
-//         console.log("hello")
-//         $('div.one-third.column').animate({
-//             width: "0"
-//         }, 500, function() {
-//             $('div.one-third.column').css("display", "none")
-//             // Animation complete.
-//         });
-//     }
-// }); // closes #tenMinutes click
-//
-//
-// // most read  click
-//
-// $("#mostRead").on("click", function(){
-//     console.log(this)
-//     if ($("h1").hasClass("MostRead") == true){
-//         $('div.one-third.column').animate({
-//             width: "30.6666666667%"
-//         }, 500, function() {
-//             $('div.one-third.column').css("display", "block")
-//             // Animation complete.
-//         });
-//     }
-//     if ($("h1").hasClass("MostRead") != true) {
-//         console.log("hello")
-//         $('div.one-third.column').animate({
-//             width: "0"
-//         }, 500, function() {
-//             $('div.one-third.column').css("display", "none")
-//             // Animation complete.
-//         });
-//     }
-// }); // closes #mostRead click
-
-
-
-
-
+        }); // closes h1.each function
+    }); // closes columns click function
 
 }); // closes document.ready
