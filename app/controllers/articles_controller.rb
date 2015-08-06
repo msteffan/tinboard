f5e34037ec5bb520db6d03210d9f83ab543629d8
Melissa Steffan <msteffan2012@gmail.com>
2015-08-06 17:42:31 -0400

    tinkering with CSS to fix the way that buttons display and the headlines scroll

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,21 +6,16 @@ class ArticlesController < ApplicationController
     def index
         if params[:query] != nil
             @guardian_article = GuardianArticle.new(params[:query])
-            # cookies[:query] = params[:query]
         end
         @articles = Article.all
         @user = User.find(session[:user]["id"])
     end
 
+#show
     def show
         @article = Article.find(params[:id])
         @user = User.find(session[:user]["id"])
         @likes = @article.likes
-    end
-
-    def guardian_show
-
-        @guardian_article = GuardianArticle.find_by(cookies[:query])
     end
 
     private
